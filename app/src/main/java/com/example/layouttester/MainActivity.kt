506cdbe1b1d03b09ebbfcc73dbc6df9e42bb46d6
Luke Yang <luke.yang@verizonmedia.com>
2020-11-02 16:37:06 -0800

    properly chained three views

--- app/src/main/java/com/example/layouttester/MainActivity.kt
+++ app/src/main/java/com/example/layouttester/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.layouttester
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.instant_promo_upsell.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toggle_share_on_snapchat.setOnClickListener {
+            three.visibility = if (three.visibility == View.VISIBLE){
+                View.GONE
+            } else {
+                View.VISIBLE
+            }
+        }
     }
 }
